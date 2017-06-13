b3ed292dc5cb303176e6bcc5235e8232cda5292a
nilankamanoj <nilankaeng16@gmail.com>
2017-06-13 21:46:23 +0530

    titles changed

--- Classes/sign-up.php
+++ Classes/sign-up.php
@@ -76,7 +76,7 @@ if(isset($_POST['btn-signup']))
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Coding Cage : Sign up</title>
+<title>Add User</title>
 <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="bootstrap/css/bootstrap-theme.min.css" rel="stylesheet" media="screen">
 <link rel="stylesheet" href="style.css" type="text/css"  />
